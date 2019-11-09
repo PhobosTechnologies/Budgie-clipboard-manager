cdcfc7ab8c2a09bf878cefcdc2dc00b461be381c
zhboner <zhboner@gmail.com>
2019-11-09 23:50:48 +1100

    Basic function done.

--- monitor.py
+++ monitor.py
@@ -3,28 +3,32 @@ gi.require_version('Budgie', '1.0')
 from gi.repository import Gtk, Gdk, GLib, Budgie
 
 class ListBoxRowWithData(Gtk.ListBoxRow):
-    def __init__(self, data):
+    def __init__(self, data, callback):
         super(Gtk.ListBoxRow, self).__init__()
         
-        
+        button = Gtk.Button(relief=Gtk.ReliefStyle.NONE)
+        button.connect("clicked", callback)
         self.label = Gtk.Label(data, halign=Gtk.Align.START, width_chars=5, max_width_chars=5)
         self.label.set_line_wrap(False)
-        self.add(self.label)
+        button.add(self.label)
+        self.add(button)
+
 
     def set_text(self, text):
         self.label.set_text(text)
 
 class Monitor(Budgie.Applet):
+    manager = None
     def __init__(self, uuid):
-
+        Budgie.Applet.__init__(self)
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
         self.HISTORY_LENGTH = 10
         self.history = []
         self.rows = []
         self.row_activated_flag = False
 
-        self.start()
         self.display()
+        self.start()
 
 
     def __update_history(self, item):
@@ -49,32 +53,47 @@ class Monitor(Budgie.Applet):
         self.clipboard.connect('owner-change',self.update_handler)
 
     def display(self):
-        self.listbox = Gtk.ListBox()
-        self.listbox.set_selection_mode(Gtk.SelectionMode.SINGLE)
-        self.listbox.set_activate_on_single_click(True)
-        self.box_outer = Gtk.EventBox()
-        self.box_outer.add(self.listbox)   
-        self.add(self.box_outer)
+        icon = Gtk.Image.new_from_icon_name(
+            "clipboard",
+            Gtk.IconSize.MENU,
+        )
+        self.icon_box = Gtk.EventBox()
+        self.icon_box.add(icon)
+        self.add(self.icon_box)
+        self.icon_box.show_all()
 
-        self.box_outer.show_all()
+        self.pop_win = Budgie.Popover.new(relative_to=self.icon_box)
+        self.pop_win.set_border_width(10)
+
+        self.listbox = Gtk.ListBox()
+        # self.listbox.set_selection_mode(Gtk.SelectionMode.SINGLE)
+        # self.listbox.set_activate_on_single_click(True)
+        
+        self.pop_win.add(self.listbox)
+        self.pop_win.get_child().show_all()
+
+        
         self.show_all()
 
-        self.listbox.connect('row-activated', self.__on_row_activated)
-        self.box_outer.connect("button-press-event", self.__on_press)
+        # self.listbox.connect('row-activated', self.__on_row_activated)
+        self.icon_box.connect("button-press-event", self.__on_press)
 
-    def __on_press(self):
-        self.box_outer.show_all()
+    def __on_press(self, widget, event_btn):
+        # self.icon_box.show_all()
+        self.manager.show_popover(self.icon_box)
 
-    def __on_row_activated(self, listbox, row):
-        idx = row.get_index()
+    def __on_row_activated(self, button):
+        idx = self.rows.index(button.get_parent())
         item = self.history[idx]
 
         self.history.pop(idx)
         self.row_activated_flag = True
         self.clipboard.set_text(item, -1)
+        
+        self.pop_win.hide()
 
     def __insert_row(self, item):
-        row = ListBoxRowWithData(item)
+        row = ListBoxRowWithData(item, self.__on_row_activated)
         self.rows.append(row)
         self.listbox.add(row)
         return row
@@ -89,5 +108,9 @@ class Monitor(Budgie.Applet):
             out = out.replace('\n', ' ')
             row.set_text(out)
         self.listbox.show_all()
+
+    def do_update_popovers(self, manager):
+        self.manager = manager
+        self.manager.register_popover(self.icon_box, self.pop_win)
 
         
